b72c5fdcff0500f4b7abe12e4fd3a6f64e670816
Xing, Wang <wang.xing.me@gmail.com>
2013-01-28 17:21:40 +0100

    complie with -g option

--- compile_and_run_newest_with_input.sh
+++ compile_and_run_newest_with_input.sh
@@ -2,8 +2,8 @@
 
 CPP=`ls -t *.cpp | head -1`
 
-# Try compilation
-g++ ./$CPP
+# Try compilation, with debug info enabled
+g++ -g ./$CPP
 if [ "$?" != "0" ]; then
     echo Complilation unsuccessful!
     exit 1
